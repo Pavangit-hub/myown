6043dbb19f1b7d029969010e16e187be7c9ae477
Pavangit-hub <93124093+Pavangit-hub@users.noreply.github.com>
2025-08-13 15:43:22 +0530

    Update ubuntu-tomcat.sh

--- ubuntu-tomcat.sh
+++ ubuntu-tomcat.sh
@@ -7,22 +7,22 @@ sudo apt update -y
 # Install Java 17 (OpenJDK)
 sudo apt install openjdk-17-jdk -y
 
-# Download Tomcat 9.0.107
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.107/bin/apache-tomcat-9.0.107.tar.gz
+# Download Tomcat 9.0.108
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.108/bin/apache-tomcat-9.0.108.tar.gz
 
 # Extract Tomcat
-tar -zxvf apache-tomcat-9.0.107.tar.gz
+tar -zxvf apache-tomcat-9.0.108.tar.gz
 
 # Add Tomcat user and roles
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.107/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.107/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="admin@123" roles="manager-gui,manager-script"/>' apache-tomcat-9.0.107/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.107/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-9.0.107/conf/tomcat-users.xml
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="admin@123" roles="manager-gui,manager-script"/>' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-9.0.108/conf/tomcat-users.xml
 
 # Remove access restrictions in Manager app context
-sed -i '21d' apache-tomcat-9.0.107/webapps/manager/META-INF/context.xml
-sed -i '22d' apache-tomcat-9.0.107/webapps/manager/META-INF/context.xml
+sed -i '21d' apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
+sed -i '22d' apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
 
 # Start Tomcat
-sh apache-tomcat-9.0.107/bin/startup.sh
+sh apache-tomcat-9.0.108/bin/startup.sh
